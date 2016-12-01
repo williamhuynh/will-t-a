6f357c75f96738ed34734f67492abb26d3d76262
William Huynh <william.huynh12@gmail.com>
2016-12-01 11:25:27 +0000

    test:

--- app/assets/javascripts/travel_maps.js
+++ app/assets/javascripts/travel_maps.js
@@ -1,36 +1,35 @@
 function initMap() {
-        directionsDisplay = new google.maps.DirectionsRenderer;
-        directionsService = new google.maps.DirectionsService;
-        
-        var something = calcRoute();
-        console.log (something);
-        
-      }
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  
 
-
-function calcRoute() {
-  var origin      = new google.maps.LatLng(41.850033, -87.6500523);
+  var origin = new google.maps.LatLng(41.850033, -87.6500523);
+  
   var destination = new google.maps.LatLng(42.850033, -85.6500523);
+  
   var request = {
       origin:      origin,
       destination: destination,
       travelMode:  google.maps.TravelMode.DRIVING
-  };
-  
-  var response = directionsService.route(request, function(response, status) {
+      };
+
+
+  directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
-      var something = response;
+      
       console.log(response);
         } 
     else { window.alert('Directions request failed due to ' + status);
         }
     
-    return response;
+      console.log(response.routes[0].legs[0].duration.value);
+      test += parseFloat(response.routes[0].legs[0].duration.value);
+      console.log(test);
+      return test;
   });
-  
-  console.log(response);
-  console.log(something);
-  return response;
-  
+
+console.log(test);
+
 }
+
